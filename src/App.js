f67f82bd24b7689918ab2c8f60fa4562d6d41330
linqin <linqin@example.com>
2020-08-23 00:11:22 +0800

    lin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.less';
-import { Layout, Menu, Icon, Row, Col, Popover, Avatar } from 'antd';
+import { Layout, Menu, Icon, Row, Col, Dropdown, Avatar } from 'antd';
 import { observer } from 'mobx-react'
 import stores from './stores'
 import { withRouter, Router, Route, Switch } from "react-router-dom";
@@ -9,6 +9,7 @@ import routers from './router'
 
 import Logo from './static/img/brand_logo.svg'
 import SLogo from './static/img/brand_logo_small.svg'
+import GuestImg from './static/img/guest_avatar.jpg'
 const history = createBrowserHistory();
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
@@ -32,21 +33,44 @@ class App extends React.Component {
 
     render() {
 
+        // 已登录菜单
+        const menu = (
+            <Menu>
+              <Menu.Item key="0">
+                <a href="http://www.alipay.com/">1st menu item</a>
+              </Menu.Item>
+              <Menu.Item key="1">
+                <a href="http://www.taobao.com/">2nd menu item</a>
+              </Menu.Item>
+              <Menu.Divider />
+              <Menu.Item key="3">3rd menu item</Menu.Item>
+            </Menu>
+          );
 
         return (
             <Router history={history}>
                 <Layout className="App">
                     <Header className="App__header">
-                        <Row justify={'space-between'}>
-                            <Col span={0} xs={4}>
+                        <Row justify={'space-between'} align={'middle'}>
+                            <Col sm={0} xs={4}>
                                 <img alt="EAPtalk Logo Small" className="brand-small-logo"
                                     src={SLogo} />
                             </Col>
-                            <Col span={4} xs={0}>
-                                <img alt="EAPtalk Logo"  className="brand-logo" id="ribbon-logo"
-                                     src={Logo} />
+                            <Col sm={4} xs={0}>
+                                <img alt="EAPtalk Logo" className="brand-logo" id="ribbon-logo"
+                                    src={Logo} />
                             </Col>
-                            <Col span={4}></Col>
+                            <Col span={12} className="header-login-wrap">
+                               
+                                    <img className="login-guest-avatar"
+                                        src={GuestImg} />
+                                
+                                <Dropdown overlay={menu} trigger={['click']}>
+                                    <a className="ant-dropdown-link dropdown-toggle" onClick={e => e.preventDefault()}>
+                                        Guest
+                                     </a>
+                                </Dropdown>
+                            </Col>
                         </Row>
 
 
